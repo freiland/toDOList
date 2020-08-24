99c799f12f8dd7cd5746277ab37ddec2b5907ff4
Daniel Schaaf <daniel.schaaf@outlook.com>
2020-08-24 11:36:47 -0700

    it will add functionality to create a list.
    Co-authored by: Forrest Reiland <freiland@email.arizona.edu>

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,38 @@
 function toDo (task,notes){
   this.task = task;
   this.notes = notes;
+  this.complete = false;
+}
+function listArr(){
+  this.list = [];
+  this.currentId= 0;
+}
+list.prototype.addItem = function(item){
+  item.id =this.assignId();
+  this.list.push(id);
+}
+list.prototype.assignId()= function(){
+  this.currentId += 1;
+  return this.currentId;
+}
+
+list.prototype.removeItem = function(id) {
+  for(let i = 0; i< this.list.length; i++){
+    if(this.list[i]==id){
+      delete this.list[i];
+      return true;
+    }
+  }
+  return false;
+}
+list.prototype.findItem = function(id){
+  for(let i =0; i < this.list.length;i++){
+    if(this.list[i].id==id){
+      return this.list[i];
+    }
+  };
+  return false;
+}
+list.prototype.complete = function(){
+  
 }
